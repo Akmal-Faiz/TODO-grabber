57a6f0c05b3761bd4960c0cfdefa4c6ab8c001e6
akmal <abinomar@dxc.com>
2021-02-15 14:12:09 +0800

    implemented functions

--- todoGrabber.py
+++ todoGrabber.py
@@ -1,20 +1,46 @@
 import glob
+import os
 
 # list all files in this and all children directories
-def listFiles(path):
-    return []
+def listFiles(path='./'):
+    res = []
+    
+    # if current path is a file, return it
+    if os.path.isfile(path):
+        return [path]
 
+    # skip this file
+    if 'todoGrabber' in path:
+        return res
+
+    children = (glob.glob(path+'/*'))
+    
+    for child in children:
+        if 'todoGrabber.py' in child:
+            continue # ignore this file
+        res += listFiles(child)
+    return res
+    
 # check if a file contains the string "TODO"
 def checkTODO(filePath):
-    pass
+    try:
+        s = open(filePath).read()
+    except UnicodeDecodeError: 
+        return False #skip non-text files
+    if 'TODO' in s:
+        return True
+    return False
 
-def grabTODOs(path = './'):
+def grabTODOs(path = os.getcwd()):
     res = []
     files = listFiles(path)
+    
     for f in files:
         if checkTODO(f):
             print(f)
+            res.append(f)
     return res
 
 if __name__ == '__main__':
-    grabTODOs()
+    curr_dir = os.sep.join(os.path.realpath(__file__).split(os.sep)[:-1])
+    grabTODOs(path=curr_dir)
--- todoGrabberTest.py
+++ todoGrabberTest.py
@@ -2,32 +2,40 @@ from todoGrabber import *
 import os
 
 # listFiles test suite
+cwd = os.getcwd()
 lf = {
     'test/fail.txt',
     'test/test.txt',
+    'test/todoGrabber.py',
     'test/testChild/fail.js',
     'test/testChild/test.js',
     'test/testChild/testGrandChild/fail.py',
-    'test/testChild/testGrandChild/test.py'
+    'test/testChild/testGrandChild/test.py',
+    'test/testChild/testGrandChild/empty'
 }
 lf_windows = {
     'test\\fail.txt',
     'test\\test.txt',
+    'test\\todoGrabber.py',
     'test\\testChild\\fail.js',
     'test\\testChild\\test.js',
     'test\\testChild\\testGrandChild\\fail.py',
-    'test\\testChild\\testGrandChild\\test.py'
+    'test\\testChild\\testGrandChild\\test.py',
+    'test\\testChild\\testGrandChild\\empty'
 }
-
+lf = {cwd + '/' + i for i in lf}
+lf_windows = {cwd + '\\' + i for i in lf_windows}
 
 # checkTODO test suite
 check = {
     'test/fail.txt': False,
     'test/test.txt': True,
+    'test/todoGrabber.py': True,
     'test/testChild/fail.js': False,
     'test/testChild/test.js': True,
     'test/testChild/testGrandChild/fail.py': False ,
-    'test/testChild/testGrandChild/test.py': True
+    'test/testChild/testGrandChild/test.py': True,
+    'test/testChild/testGrandChild/empty': False
 }
 
 # grabTODOs test suite
@@ -36,25 +44,25 @@ gtd = {
     'test/testChild/test.js',
     'test/testChild/testGrandChild/test.py'
 }
-
 gtd_windows = {
     'test\\test.txt',
     'test\\testChild\\test.js',
     'test\\testChild\\testGrandChild\\test.py'
 }
+gtd = {cwd + '/' + i for i in gtd}
+gtd_windows = {cwd + '\\' + i for i in gtd_windows}
 
 if __name__ == '__main__':
-    
     if os.name != 'nt':
-        assert(set(listFiles('./test/')) == lf),'listFiles test failed'
+        assert(set(listFiles(cwd + '/test/')) == lf),'listFiles test failed'
     else:
-        assert(set(listFiles('./test/')) == lf_windows),'listFiles test failed'
+        assert(set(listFiles(cwd + '\\test\\')) == lf_windows),'listFiles  test failed'
     
     for f in check:
         assert(checkTODO(f) == check[f]), 'checkTODO  test failed for %s' % f
     
     if os.name != 'nt':
-        assert(set(grabTODOs('./test/')) == gtd),'grabTODOs test failed'
+        assert(set(grabTODOs(cwd + '/test/')) == gtd),'grabTODOs test failed'
     else:
-        assert(set(grabTODOs('./test/')) == gtd_windows),'grabTODOs test failed'
+        assert(set(grabTODOs(cwd + '\\test\\')) == gtd_windows),'grabTODOs test failed'
     
